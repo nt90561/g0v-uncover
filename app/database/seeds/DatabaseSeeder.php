dddc1ae1e3211ea148fef4cd20e05348f54e015c
meisheep <immeisheep@gmail.com>
2015-01-14 17:50:02 +0800

    Delete old seeder

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,8 +11,6 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		// old
-		// $this->call('TruenameTableSeeder');
 		$this->call('FirstSectionTableSeeder');
 	}
 
@@ -85,70 +83,3 @@ class FirstSectionTableSeeder extends Seeder {
 		fclose($file);
 	}
 }
-
-
-// old version
-// class TruenameTableSeeder extends Seeder {
-
-// 	public function run()
-// 	{
-// 		if(!Schema::hasTable('judge')) {
-// 			Schema::create('judge',function($table)
-// 			{
-// 				$table->increments('id');
-// 				$table->string('name');
-// 				$table->integer('year');
-// 				$table->string('case');
-// 				$table->integer('no');
-// 				$table->string('date', 7);
-// 				$table->string('cause');
-// 			});
-// 		}
-
-// 		// file path
-// 		$file_array = file('app/database/seeds/output.txt');
-// 		$count = 0;
-
-// 		$name_array;
-// 		$case_array;
-// 		$date;
-// 		$cause;
-// 		foreach($file_array as $key => $value)
-// 		{
-// 		    if(($key % 4) == 0) {
-// 		        $name_array = explode(', ', str_replace('"', '', substr(trim($value), strpos($value, '[') + 1, -2)));
-// 		    } else if(($key % 4) == 1) {
-// 		        $case_array = explode(',', substr(trim($value), strpos($value, '"') + 1, -2));
-// 		    } else if(($key % 4) == 2) {
-// 		        $date = substr($value, strpos(trim($value), '"') + 1, -2);
-// 		    } else if(($key % 4) == 3) {
-// 		        $cause = substr($value, strpos(trim($value), '"') + 1, -2);
-// 		        foreach($name_array as $name) {
-// 		        	// for console, not necessary
-// 		        	print_r(array(
-// 	        			'name' => $name,
-// 	        			'year' => $case_array[0],
-// 	        			'case' => $case_array[1],
-// 	        			'no' => $case_array[2],
-// 	        			'date' => $date,
-// 	        			'cause' => $cause
-// 		        	));
-
-// 		        	// insert
-// 		        	DB::table('judge')->insert(
-// 		        		array(
-// 		        			'name' => $name,
-// 		        			'year' => $case_array[0],
-// 		        			'case' => $case_array[1],
-// 		        			'no' => $case_array[2],
-// 		        			'date' => $date,
-// 		        			'cause' => $cause
-// 		        		)
-// 		        	);
-// 		        }
-// 		    }
-
-// 		}
-
-// 	}
-// }
