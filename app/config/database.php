905bba41b3a91e815de9c728c425a77758be8fa0
meisheep <immeisheep@gmail.com>
2015-01-14 17:55:26 +0800

    Fix database.php

--- app/config/database.php
+++ app/config/database.php
@@ -1,5 +1,12 @@
 <?php
 
+$url = parse_url(getenv("DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
 return array(
 
 	/*
@@ -43,13 +50,6 @@ return array(
 	| choice installed on your machine before you begin development.
 	|
 	*/
-
-	$url = parse_url(getenv("DATABASE_URL"));
-
-	$host = $url["host"];
-	$username = $url["user"];
-	$password = $url["pass"];
-	$database = substr($url["path"], 1);
 
 	'connections' => array(
 
